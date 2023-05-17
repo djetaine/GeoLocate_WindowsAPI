66d97c62976180b28abf63830c54c23f45b00e23
jlaprade-git <joel.laprade@gmail.com>
2023-05-17 13:18:01 -0500

    Update GeoLocate.ps1
    Fixed bad URL creation in NinjaRMM field update

--- GeoLocate.ps1
+++ GeoLocate.ps1
@@ -45,7 +45,7 @@ if ($GeoLocationLatLong.Permission -eq 'Denied'){
     $latitude = $GeoLocationLatLong.Position.Location.Latitude
     $longitude = $GeoLocationLatLong.Position.Location.Longitude
 
-    $apiKey = '<your OpenCage API Key' # Replace with your OpenCage API key
+    $apiKey = '<your OpenCage API Key>' # Replace with your OpenCage API key
     $url = "https://api.opencagedata.com/geocode/v1/json?q=$latitude+$longitude&key=$apiKey"
 
     $response = Invoke-RestMethod -Uri $url
@@ -57,7 +57,13 @@ if ($GeoLocationLatLong.Permission -eq 'Denied'){
         Write-Error 'Failed to retrieve address'
     }
 }
-#This sets updates a custom field in NinjaRMM to display the address and create a link to google maps
-#Ninja-Property-Set Address $Address
+
+# Uncomment this if you use this script in conjunction with NinjaRMM
+# The following updates a custom field in NinjaRMM to display the address and create a link to google maps. 
+# Create this custom field in Administration > Devices > Global Custom Fields with the Name matching the name here. 
+# Address is a Text Field type, Url is a URL Field type.
+# Once created set the technician field to Read Only and the Scripts field to Write. Set to Read/Write if you plan on using this value in further scripts. 
+
+#Ninja-Property-Set -name Address -value $Address
 #$GoogleMapsLink = "https://www.google.com/maps/@$latitude,$longitude"
-#Ninja-Property-Set GoogleMapsLink = $GoogleMapsLink
+#Ninja-Property-Set -name GoogleMapsurl -value $GoogleMapsLink
